016502b0e42b91d6ff510434f0fed2b6324cba23
Shakti Gupta <74551736+skcse123@users.noreply.github.com>
2022-01-15 21:33:31 +0530

    Add files via upload

---
+++ B.tutorials/react.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+//function Greet(){
+  //  return <h1>functionally implement</h1>
+//} OR
+const Greet = () => <h1>functionally implement</h1>
+
+export default Greet
---
+++ B.tutorials/reactnative.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {View,Text} from 'react-native'
+const App =()=>{
+    return(
+        <View>
+            <Text style={{fontSize:60}}>functionally implement</Text>
+        </View>
+    )
+}
+
+export default App
